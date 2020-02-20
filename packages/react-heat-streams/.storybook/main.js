1be9ec37e07d5b8e44cf92a35a07eab6e5596bd0
Chris Trevino <chtrevin@microsoft.com>
2020-02-19 16:11:35 -0800

    fix: move stories to typescript

--- packages/react-heat-streams/.storybook/main.js
+++ packages/react-heat-streams/.storybook/main.js
@@ -1,9 +1,24 @@
 module.exports = {
-	stories: ['../stories/**/*.stories.js'],
+	stories: ['../stories/**/*.stories.tsx', '../stories/**/*.stories.ts'],
 	addons: [
-    '@storybook/preset-typescript',
+		'@storybook/preset-typescript',
 		'@storybook/addon-actions',
 		'@storybook/addon-links',
 		'@storybook/addon-a11y',
 	],
+	webpackFinal: async config => {
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			use: [
+				{
+					loader: require.resolve('ts-loader'),
+				},
+				{
+					loader: require.resolve('react-docgen-typescript-loader'),
+				},
+			],
+		})
+		config.resolve.extensions.push('.ts', '.tsx')
+		return config
+	},
 }
