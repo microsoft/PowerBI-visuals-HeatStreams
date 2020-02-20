679df29358119745a5c0d7c42d5defa855e254b1
Chris Trevino <chtrevin@microsoft.com>
2020-02-20 13:39:56 -0800

    build: update powerbi settings

--- powerbi-heat-streams/src/settings/VisualSettings.ts
+++ powerbi-heat-streams/src/settings/VisualSettings.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 /* eslint-disable import/no-webpack-loader-syntax */
-import { IVisualDataOptions, IVisualRenderingOptions } from '../settings/types'
+import { IVisualDataOptions, IVisualRenderingOptions } from './types'
 import { VisualDataOptionsImpl } from './VisualDataOptionsImpl'
 import { VisualRenderingOptionsImpl } from './VisualRenderingOptionsImpl'
 import { dataViewObjectsParser } from 'powerbi-visuals-utils-dataviewutils'
