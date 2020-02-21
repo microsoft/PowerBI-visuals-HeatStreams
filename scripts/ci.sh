7586e0cb22edf21879c5695ee83e6e958297adc0
Chris Trevino <chtrevin@microsoft.com>
2020-02-21 12:16:51 -0800

    build: update CI script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 set -e
 
-npm install
-npm run audit
-
 NODE_ENV=production yarn npm-run-all clean -p lint check_formatting
 yarn lerna run build --stream
 
 pushd pbi-heat-streams
+npm install
+npm run audit
 npm run lint 
 npm run package
 
