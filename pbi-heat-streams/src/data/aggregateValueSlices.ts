ef910678519f16931763aafad063e804666b0102
Chris Trevino <chtrevin@microsoft.com>
2020-02-20 18:05:48 -0800

    refactor: comment, cleanup

--- pbi-heat-streams/src/data/aggregateValueSlices.ts
+++ pbi-heat-streams/src/data/aggregateValueSlices.ts
@@ -12,26 +12,13 @@ import {
 import { mean } from 'd3-array'
 import { ICategoryDataMap } from '../chart/types'
 
-function sliceStart(date: Date, dateAggregation: DateAggregation): Date {
-	const result = new Date(date)
-	result.setUTCMilliseconds(0)
-	result.setUTCSeconds(0)
-	result.setUTCMinutes(0)
-
-	if (dateAggregation === DateAggregation.Days) {
-		result.setUTCHours(0)
-	} else if (dateAggregation === DateAggregation.Months) {
-		result.setUTCHours(0)
-		result.setUTCDate(1)
-	} else if (dateAggregation === DateAggregation.Years) {
-		result.setUTCHours(0)
-		result.setUTCDate(1)
-		result.setUTCMonth(1)
-	}
-
-	return result
-}
-
+/**
+ * Aggregates values into buckets using aggregation configs
+ * @param data The raw data map
+ * @param positionDomain The x domain
+ * @param dateAggregation The date aggregation to use (if x is dates)
+ * @param numericAggregation The numeric aggregation to use (if x is numbers)
+ */
 export function aggregateValueSlices(
 	data: ICategoryDataMap,
 	positionDomain: XDomain,
@@ -93,4 +80,24 @@ export function aggregateValueSlices(
 	)
 
 	return [result, [valueMin, valueMax]]
+}
+
+function sliceStart(date: Date, dateAggregation: DateAggregation): Date {
+	const result = new Date(date)
+	result.setUTCMilliseconds(0)
+	result.setUTCSeconds(0)
+	result.setUTCMinutes(0)
+
+	if (dateAggregation === DateAggregation.Days) {
+		result.setUTCHours(0)
+	} else if (dateAggregation === DateAggregation.Months) {
+		result.setUTCHours(0)
+		result.setUTCDate(1)
+	} else if (dateAggregation === DateAggregation.Years) {
+		result.setUTCHours(0)
+		result.setUTCDate(1)
+		result.setUTCMonth(1)
+	}
+
+	return result
 }
